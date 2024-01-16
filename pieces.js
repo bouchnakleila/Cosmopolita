c49397e973378ed732283c3e46ed918b70fb91df
bouchnakleila <107683569+bouchnakleila@users.noreply.github.com>
2024-01-16 21:34:01 +0100

    Update pieces.js

--- pieces.js
+++ pieces.js
@@ -4,7 +4,7 @@ let pieces = window.localStorage.getItem('pieces');
 
 if (pieces === null){
     // Récupération des pièces depuis l'API
-    const reponse = await fetch('https://bouchnakleila.github.io/Cosmopolita/pieces/');
+    const reponse = await fetch('https://bouchnakleila.github.io/Cosmopolita/api-http/db.json/pieces/');
     pieces = await reponse.json();
     // Transformation des pièces en JSON
     const valeurPieces = JSON.stringify(pieces);
